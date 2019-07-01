7f750ff2b062837397ac63781564fba28491eee5
IbrahimOmar91 <46249457+IbrahimOmar91@users.noreply.github.com>
2019-07-01 19:56:46 +0200

    Add files via upload

---
+++ src/quiz/cluster/build/CMakeFiles/quizCluster.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quizCluster.dir/cluster.cpp.o"
+  "CMakeFiles/quizCluster.dir/home/hema/SFND/01Lidar/SFND_Lidar_Obstacle_Detection/src/render/render.cpp.o"
+  "quizCluster.pdb"
+  "quizCluster"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quizCluster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
